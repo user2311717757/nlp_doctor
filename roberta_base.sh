e0774ef1e02c9bcf839ebc620c5fd24d9892fe3f
user2311717757 <129850873+user2311717757@users.noreply.github.com>
2023-06-07 14:15:42 +0800

    Update roberta_base.sh

--- roberta_base.sh
+++ roberta_base.sh
@@ -5,7 +5,6 @@ export TASK_NAME=mrpc
 #export TASK_NAME=yelp_polarity
 #--dataset_name $TASK_NAME \
 #--task_name $TASK_NAME \
-export path="/root/dataln/nianke_disentangled/transformers/examples/pytorch/text-classification/result/roberta_black_shadow_targetmodel_mrpc/checkpoint-100"
 CUDA_VISIBLE_DEVICES=0,2,3 python run_glue.py \
   --model_name_or_path roberta-base \
   --task_name $TASK_NAME \
