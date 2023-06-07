4782f7eb2df31e4530f915b257158ba306d7cd8c
user2311717757 <129850873+user2311717757@users.noreply.github.com>
2023-06-07 14:15:23 +0800

    Update gpt2_medium.sh

--- gpt2_medium.sh
+++ gpt2_medium.sh
@@ -5,7 +5,6 @@ export TASK_NAME=mrpc
 #export TASK_NAME=yelp_polarity
 #--dataset_name $TASK_NAME \
 #--task_name $TASK_NAME \
-export path="/root/dataln/nianke_disentangled/transformers/examples/pytorch/text-classification/result/gpt2_shadow_black_targetmodel_mrpc/checkpoint-100"
 CUDA_VISIBLE_DEVICES=1,2,3 python run_glue.py \
   --model_name_or_path gpt2-medium \
   --task_name $TASK_NAME \
